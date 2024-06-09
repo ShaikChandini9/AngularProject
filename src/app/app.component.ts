1ccad9cdd0ac68503614f0c0614e241acaf1bdfb
ShaikChandini9 <chandinishaik923@gmail.com>
2024-06-09 17:32:03 -0500

    modifying code inorder to display the message
    code to display the message in the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from "./user/user.component";
 import { ProductComponent } from "./product/product.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, UserComponent, ProductComponent]
+    imports: [RouterOutlet, UserComponent, ProductComponent,FormsModule]
 })
 export class AppComponent {
   title = '';
+  message: any;
 }
